a6420516512f264527b26f7a0b943b8d86d61828
Shihan Li <57557675+Shihan150@users.noreply.github.com>
2025-12-23 22:50:59 -0600

    Add files via upload

---
+++ hprc_job_code/16_pco2_850/main_job.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=Early_PTB_Sr_temp_d13c_sens_pco2_850
+#SBATCH --time=900:00
+#SBATCH --ntasks=1
+
+#SBATCH --cpus-per-task=48
+#SBATCH --mem=25G
+#SBATCH --output=%x_%a.out
+
+
+#SBATCH --mail-user=shihan@tamu.edu
+#SBATCH --mail-type=END
+
+# JOB HEADERS HERE
+
+# some job information
+echo "Starting at $(date)"
+echo "Job submitted to the ${SLURM_JOB_PARTITION} partition, the default partition on ${SLURM_CLUSTER_NAME}"
+echo "Job name: ${SLURM_JOB_NAME}, Job ID: ${SLURM_JOB_ID}"
+echo "I have ${SLURM_CPUS_ON_NODE} CPUs and ${mem_gbytes} GiB of RAM on compute node $(hostname)"
+
+# echo the current zone
+echo this is array job id $SLURM_ARRAY_JOB_ID
+echo this is array task id $SLURM_ARRAY_TASK_ID
+
+printf "\n\n"
+
+module load Anaconda3/2024.02-1
+# conda install python=3.9
+python mcmc_temp_job_pco2_850.py
